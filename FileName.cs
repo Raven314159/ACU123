fab4a2ccef772fcb128cbb563f068532514b412e
Raven314159 <99815390+Raven314159@users.noreply.github.com>
2024-10-17 12:59:23 +0800

    飞行棋小游戏
    拿来做实验用的

---
+++ FileName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXP10GameBuilder
+{
+    public class FileName
+    {
+        public string _name;
+        public string Name//属性
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+
+
+        public int _age;
+        public char _gender;
+
+        public static void CHLSS()
+        {
+            Console.WriteLine("xxxxxx");
+        }
+    }
+}
